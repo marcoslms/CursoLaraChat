7bba7d4eb28f2ecad584073c67c37f105e7bbe45
marcoslms <marcoslms25@gmail.com>
2023-07-27 18:12:37 -0300

    teste 2

--- config/app.php
+++ config/app.php
@@ -53,6 +53,7 @@ return [
     | the Artisan command line tool. You should set this to the root of
     | your application so that it is used when running Artisan tasks.
     |
+    teste
     */
 
     'url' => env('APP_URL', 'http://localhost'),
